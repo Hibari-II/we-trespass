9656ed3bc7d6af4fd97689f68ef9f203089ab161
Hoa Binh Dinh <h.dinh@live.at>
2021-09-29 12:12:16 +0200

    basic dog api fetch

--- src/scripts/api-consumption.ts
+++ src/scripts/api-consumption.ts
@@ -1,1 +1,24 @@
-console.log("Api")
+enum apiClasses {
+    tableHead = "petshop__list__headings",
+    tableBody = "petshop__list__data",
+    tableRow = "petshop__list__row",
+    tableCell = "petshop__list__row__cell",
+}
+
+const dogs: String[] = []; 
+
+
+fetch("https://dog.ceo/api/breeds/list/all")
+.then(response => response.json())
+.then(responseJson => {
+    const respondingBreeds= responseJson.message;
+    for (const breed in respondingBreeds) {
+        if (respondingBreeds[breed].length) {
+            responseJson.message[breed].forEach((subBreed: String) => 
+                dogs.push(`${breed} ${subBreed}`)
+            );
+            continue;
+        }
+        dogs.push(breed);
+    }
+});
