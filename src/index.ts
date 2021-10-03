818e628a9e5b5a00e5897b8d0f3e293859a54b92
Hoa Binh Dinh <h.dinh@live.at>
2021-10-03 23:20:33 +0200

    about-me page

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import "./main.scss";
 import "./assets/logo.svg";
 import "./assets/welcome.svg";
 import "./assets/contact.svg";
+import "./assets/wave.svg";
 
 // Scripts 
 import "./scripts/navigation";
